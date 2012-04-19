4e3ee391b719d219db24650006003cc58bc25648
Laurent Jouanneau <ljouanneau@gmail.com>
2012-04-20 01:22:44 +0200

    Update jelix

--- lib/installwizard/jtpl/jTpl.class.php
+++ lib/installwizard/jtpl/jTpl.class.php
@@ -16,7 +16,7 @@
  */
 class jTpl {
 
-    const VERSION = '1.0pre.2325';
+    const VERSION = '1.0pre.2332';
 
     /**
      * all assigned template variables. 
--- lib/jelix/init.php
+++ lib/jelix/init.php
@@ -17,7 +17,7 @@
  * Version number of Jelix
  * @name  JELIX_VERSION
  */
-define ('JELIX_VERSION', '1.4pre.2325');
+define ('JELIX_VERSION', '1.4pre.2332');
 
 /**
  * base of namespace path used in xml files of jelix
